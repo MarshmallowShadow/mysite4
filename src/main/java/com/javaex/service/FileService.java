12c849de2bea1e060cdb3d32c429101b7a150054
MarshmallowShadow <choiboseung@naver.com>
2022-06-30 16:08:51 +0900

    2022-06-30

--- src/main/java/com/javaex/service/FileService.java
+++ src/main/java/com/javaex/service/FileService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.dao.FileDao;
+import com.javaex.vo.FileVo;
 
 @Service
 public class FileService {
@@ -16,15 +17,26 @@ public class FileService {
 	public void save(MultipartFile file) {
 		System.out.println("FileService: save");
 		
+		//orgName
 		String orgName = file.getOriginalFilename();
+		
+		//saveName
 		String exName = orgName.substring(orgName.lastIndexOf(".")); //파일 형식
 		String rand1 = UUID.randomUUID().toString(); //1차겹침방지
 		long rand2 = System.currentTimeMillis(); //2차겹침방지
 		String saveName = rand1 + rand2 + exName;
-		System.out.println(saveName);
 		
+		//filePath
 		String saveDir = "C:\\javastudy\\fileupload"; //갤러리 실제 파일 경로
 		String filePath = saveDir + "\\" + saveName;
+		
+		//fileSize
+		long fileSize = file.getSize();
+		
+		//Vo에 저장
+		FileVo fVo = new FileVo(orgName, saveName, filePath, fileSize);
+		System.out.println(fVo);
+		
 		
 	}
 }
