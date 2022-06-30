6cf11abbb3c3708837ada1f32c51406e1c20de4c
MarshmallowShadow <choiboseung@naver.com>
2022-06-30 15:59:20 +0900

    2022-06-30

--- src/main/java/com/javaex/service/FileService.java
+++ src/main/java/com/javaex/service/FileService.java
@@ -17,7 +17,14 @@ public class FileService {
 		System.out.println("FileService: save");
 		
 		String orgName = file.getOriginalFilename();
-		String saveName = UUID.randomUUID().toString();
+		String exName = orgName.substring(orgName.lastIndexOf(".")); //파일 형식
+		String rand1 = UUID.randomUUID().toString(); //1차겹침방지
+		long rand2 = System.currentTimeMillis(); //2차겹침방지
+		String saveName = rand1 + rand2 + exName;
 		System.out.println(saveName);
+		
+		String saveDir = "C:\\javastudy\\fileupload"; //갤러리 실제 파일 경로
+		String filePath = saveDir + "\\" + saveName;
+		
 	}
 }
